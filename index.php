c84b06b49151d62315cce73e2e65ebe38d509785
Y.Dziashkevich <Y.Dziashkevich@gmail.com>
2014-07-31 16:18:52 +0300

    add methods getRootElement, getChildElement

--- index.php
+++ index.php
@@ -1,17 +1,52 @@
 <?php
-header ("Content-Type: text/html; charset=utf-8");
-echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">
-<html>
-<head>
-<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\">
-<title>Open Server</title>
-</head>
-<body>
-<center>
-<br><br><br><div style=\"width: 600px;\"><span style=\"font-size: 32px; color: green; font-family: Arial, Verdana;\">Добро пожаловать в Open Server!</span>
-<br><br><br><span style=\"font-size: 18px; color: #333; font-family: Verdana, Arial;\">Вау! Он работает ;-)</span>
-<br><br><br><br><span style=\"font-size: 12px; color: #333; font-family: Verdana, Arial;\">Перед использованием программы ознакомьтесь с руководством пользователя!<br>(Меню - Дополнительно - Документация)</span></div>
-</center>
-</body>
-</html>";
-?>
+
+require_once("./inc/inc.php");
+date_default_timezone_set('UTC');
+
+$form=new Form();
+$storage=new Storage();
+$tree=new Tree();
+
+$element=array();
+$page="";
+$array=array();
+$htmlTree="";
+
+
+if(!empty($_POST) && isset($_POST["newElement"]) && isset($_POST["parent"]) && $_POST["parent"]!=" "){
+    $array["add"]="add";
+    $array["parent"]=$_POST["parent"];
+    $page=$form->getPage($array);
+}else{
+    $page=$form->getPage();
+}
+
+if(isset($_POST["nameElement"])){
+    foreach($form->getData() as $key=>$value){
+        $element[$key]=$value;
+    }
+}
+
+if(isset($element[":nameElement"]) && $element[":nameElement"]!=" "){
+    $storage->putElement($element);
+}
+
+
+foreach($storage->getRootElements() as $item){
+    $htmlTree="<ul>";
+    $htmlTree.="<li>";
+    $htmlTree.="$item[name]";
+    $htmlTree.="<ul>";
+    foreach($storage->getChild($item["name"]) as $item){
+
+
+    }
+    $htmlTree.="</ul>";
+    $htmlTree.="</li>";
+    $htmlTree.="</ul>";
+}
+
+
+
+
+echo $page;
