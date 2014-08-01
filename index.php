6e4dc39c734db40b3436193d2c35494316b2ed63
Y.Dziashkevich <Y.Dziashkevich@gmail.com>
2014-08-01 13:24:18 +0300

    select
    select

--- index.php
+++ index.php
@@ -76,18 +76,11 @@ foreach($storage->getRootElements() as $rootElement){
 $htmlSelect="<select>";
 $level="-";
 foreach($storage->getRootElements() as $rootElement){
-<<<<<<< HEAD
     var_dump($rootElement["name"]);
     $child=array();
     $htmlSelect.="<optin value='$rootElement[name]'>";
     $htmlSelect.=$rootElement["name"];
     $htmlSelect.="</option>";/*
-=======
-    $child=array();
-    $htmlSelect.="<optin value='$rootElement[name]'>";
-    $htmlSelect.=$rootElement["name"];
-    $htmlSelect.="</option>";
->>>>>>> origin/master
     $child=$storage->getChild($rootElement["id"]);
     if(!empty($child["id"])){
         $htmlSelect.="<option value='$child[name]'>";
@@ -106,11 +99,7 @@ foreach($storage->getRootElements() as $rootElement){
             $htmlSelect.="</option>";
         }
 
-<<<<<<< HEAD
     }*/
-=======
-    }
->>>>>>> origin/master
 }
 $htmlSelect.="</select>";
 
